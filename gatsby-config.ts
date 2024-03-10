6df785d6522fc5af74cfafdd852bb45d8e73b1cb
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-03-10 15:13:07 +0000

    update css loader

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,25 +3,37 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `26pairsTS`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-transformer-typescript-css-modules", "gatsby-plugin-image",'gatsby-plugin-react-helmet','gatsby-plugin-open-graph-images', "gatsby-plugin-sitemap","gatsby-plugin-dark-mode",  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/assets/icons/26pairsFavicon.jpeg"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-transformer-typescript-css-modules",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-open-graph-images",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-dark-mode",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/assets/icons/26pairsFavicon.jpeg",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
 
 export default config;
