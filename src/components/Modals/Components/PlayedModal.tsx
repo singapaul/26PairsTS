f90a7bfb72f78bde7b9e01165468f272a8126946
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-03-28 22:22:59 +0000

    Fix issue

--- src/components/Modals/Components/PlayedModal.tsx
+++ src/components/Modals/Components/PlayedModal.tsx
@@ -106,7 +106,7 @@ export const PlayedModal = ({
         <div>
           <button
             type="button"
-            onClick={() => () => navigate(classicShuffle)}
+            onClick={() => navigate(classicShuffle)}
             className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base"
           >
             Classic Shuffle
