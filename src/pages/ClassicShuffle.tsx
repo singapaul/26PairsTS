b585daeb6f014ce0cd8fda2982a6604f1d2f78a5
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-04-15 19:40:25 +0100

    Add google analytics tags

--- src/pages/ClassicShuffle.tsx
+++ src/pages/ClassicShuffle.tsx
@@ -18,3 +18,45 @@ const ClassicShuffle = ({ path }: { path: string }) => {
 };
 
 export default ClassicShuffle;
+
+export function Head() {
+  return (
+    <>
+      <meta property="og:title" content="26Pairs" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.26pairs.com" />
+      <meta
+        property="og:image"
+        content="https://www.26pairs.com/images/OpenGraph"
+      />
+      <meta
+        property="og:description"
+        content="Join the fun and challenge your memory with 26Pairs, the ultimate card matching game!"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      {/* <meta name="twitter:creator" content="@YourTwitterHandle" /> */}
+      <meta name="twitter:title" content="26Pairs" />
+      <meta
+        name="twitter:description"
+        content="Join the fun and challenge your memory with 26Pairs, the ultimate card matching game!"
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.26pairs.com/og-image.jpg"
+      />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-WVXZRW28SC"
+      ></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WVXZRW28SC')`}
+        ;
+      </script>
+    </>
+  );
+}
